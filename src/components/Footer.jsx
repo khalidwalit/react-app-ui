f7ccc93990b6bdf27b780b133e67df92ed8bde74
Khalid Walit <harithkhalid93@gmail.com>
2022-05-11 22:02:58 +0800

    add footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import {logout, reset} from '../features/auth/authSlice'
 function Header() {
   return (
     <footer className='footer'>
-        <div>
+        <div className='container'>
           &copy; {new Date().getFullYear()} Copyright: <a href="https://www.hvrange.com"> HvRange.com </a> 
           Developed by Khalid Walit
         </div>
